8cf3e4970c98170cefdce8cdb70584d74a8e0ee9
Sajid <shaikhsajeed375@gmail.com>
2026-05-14 19:13:22 +0530

    first commit

---
+++ Basic/02_dataType.js
@@ -0,0 +1,35 @@
+//string
+let name="mohit";
+//console.log(name);
+
+console.log(typeof name);
+
+//Number
+let age=20;
+console.log(age);
+console.log(typeof age);
+
+//Boolean
+let isStudent= true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+//null
+let emptyValue=null;
+console.log(emptyValue);
+console.log(typeof emptyValue);
+
+//undefined
+let abc;
+console.log(abc);
+console.log.apply(typeof abc);
+
+//BigInt
+let bigIntvalue=123456789n;
+console.log(bigIntvalue);
+console.log(typeof bigIntvalue);
+
+//Symbol
+let symbolValue=Symbol("unique");
+console.log(symbolValue);
+console.log(typeof symbolValue);
